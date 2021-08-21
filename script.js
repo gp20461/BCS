881ad06d695fe90f44bb86f46ff40013e1a3d970
Gopal <gp20461@gmail.com>
2021-08-21 23:19:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,99 @@
+
+// Seminar
+function seminar() {
+    document.getElementById("smn").classList.toggle("show");
+    document.getElementById("intro").innerHTML = `<h5 style="color:black"; text-align:center;margin-top:4rem;>The purpose of the seminars is not to give the complete comprehensive understanding of the topic but to generate the interest so that one becomes encouraged to study those topics or those books. Actually in 3-4 days it is not possible to give the total understanding of the book. Actually what to speak of the books, just on one verse we can meditate throughout our whole life. Isn’t that the case with Bhagavad Gita? Just one verse of Bhagavad Gita, we can meditate throughout our whole life on that verse and still we may not be able to understand, still we may not be able to totally comprehend. So, that is the spiritual reality even one minute atomic particle is inconceivable. That is the spiritual reality.</h5>`;
+}
+// Art of dying
+function AOD() {
+    document.getElementById("aod").classList.toggle("show");
+}
+function aodp1(){
+    document.getElementById("intro").innerHTML = "Part1 - art of dying";
+}
+function aodp2(){
+    document.getElementById("intro").innerHTML = "Part1 - art of dying";
+}
+function aodp3(){
+    document.getElementById("intro").innerHTML = "Part1 ";
+}
+// Brahmacraya
+function brahm() {
+    document.getElementById("brahm").classList.toggle("show");
+}
+// Bhagwad Gita
+function bg() {
+    document.getElementById("bg").classList.toggle("show");
+}
+function vh() {
+    document.getElementById("vh").classList.toggle("show");
+}
+function mk() {
+    document.getElementById("mk").classList.toggle("show");
+}
+function kp() {
+    document.getElementById("kp").classList.toggle("show");
+}
+function tlc() {
+    document.getElementById("tlc").classList.toggle("show");
+}
+function ve() {
+    document.getElementById("ve").classList.toggle("show");
+}
+function ks() {
+    document.getElementById("ks").classList.toggle("show");
+}
+function ssu() {
+    document.getElementById("ssu").classList.toggle("show");
+}
+function ug() {
+    document.getElementById("ug").classList.toggle("show");
+}
+
+
+    // Bhagwad Gita classes
+function bgclass() {
+        document.getElementById("bgc").classList.toggle("show");
+        document.getElementById("intro").innerHTML ="BHagwad gita";
+    }
+    function bgclasses1(){
+        document.getElementById("intro").innerHTML = "Part2";
+    
+    }function bgclasses2(){
+        document.getElementById("intro").innerHTML = "Part3";
+    
+    }function bgclasses3(){
+        document.getElementById("intro").innerHTML = "Part4";
+    
+    }function bgclasses4(){
+        document.getElementById("intro").innerHTML = "Part5";
+    
+    }function bgclasses5(){
+        document.getElementById("intro").innerHTML = "Part6";
+    
+    }
+// Srimad Bhagwatam
+function sbclass() {
+        document.getElementById("sbc").classList.toggle("show");
+}
+function cc() {
+    document.getElementById("cc").classList.toggle("show");
+}
+function spp() {
+    document.getElementById("spp").classList.toggle("show");
+}
+function va() {
+    document.getElementById("va").classList.toggle("show");
+}
+function vpl() {
+    document.getElementById("vpl").classList.toggle("show");
+}
+function vo() {
+    document.getElementById("vo").classList.toggle("show");
+}
+function fc() {
+    document.getElementById("fc").classList.toggle("show");
+}
+function sc() {
+    document.getElementById("sc").classList.toggle("show");
+}
